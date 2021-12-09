8f06e77f667e38fa5d2552ac767e9eaa1036ab86
Bill Robbins <bill.robbins@automattic.com>
2021-12-09 11:43:32 -0600

    Initial changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,63 @@
 // Import SCSS entry file so that webpack picks up changes
+import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { ProductItem } from './ProductItem.js';
 import './index.scss';
 
-function RootComponent() {
-	return <h2>Store React app</h2>;
+class RootComponent extends Component {
+	state = {
+		canSave: false,
+		viewButtonRow: false,
+	};
+
+	viewCancelSave = () => {
+		this.setState( {
+			viewButtonRow: true,
+		} );
+	};
+
+	hideCancelSave = () => {
+		this.setState( {
+			viewButtonRow: false,
+		} );
+	};
+
+	disableSave = () => {
+		this.setState( {
+			canSave: false,
+		} );
+	};
+
+	enableSave = () => {
+		this.setState( {
+			canSave: true,
+		} );
+	};
+
+	render() {
+		return (
+			<>
+				<h2>Store React app</h2>
+				<div className="add-product">
+					<button
+						className="button button-primary"
+						onClick={ this.viewCancelSave }
+						disabled={ this.state.viewButtonRow }
+					>
+						Add Product
+					</button>
+				</div>
+				{ this.state.viewButtonRow && (
+					<ProductItem
+						canSave={ this.state.canSave }
+						disableSave={ this.disableSave }
+						enableSave={ this.enableSave }
+						hideCancelSave={ this.hideCancelSave }
+					/>
+				) }
+			</>
+		);
+	}
 }
 const domContainer = document.querySelector( '.store-wrapper' );
 render( <RootComponent />, domContainer );
